a0b1f1f78d8a9a337ba474fccb72c9df095ceae0
gilangrizkiputra <gilangrizkiptra@gmail.com>
2024-06-18 18:28:28 +0700

    fix layout bottom navigation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -82,4 +82,6 @@ dependencies {
 
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
 }
